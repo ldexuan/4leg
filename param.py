b7b627889ac39d142d2fff3373ae9443fc12d949
ldexuan <133487065+ldexuan@users.noreply.github.com>
2023-05-13 16:33:07 +0800

    Add files via upload

---
+++ param.py
@@ -0,0 +1,15 @@
+model = 'a1'
+if model == 'a1':
+    lenth_of_Hip=0.08505
+    lenth_of_Thigh=0.2
+    lenth_of_Calf=0.2
+    length_of_body=0.366
+    width_of_body=0.094
+elif model == 'aliengo':
+    pass
+elif model == 'laikago':
+    pass
+else :
+    pass
+
+param = [lenth_of_Hip,lenth_of_Thigh,lenth_of_Calf,length_of_body,width_of_body]
